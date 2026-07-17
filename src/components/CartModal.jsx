225be96a271f94397d7add3f8cef5ef0afd1d371
okturan <okanert@gmail.com>
2026-07-17 13:42:16 +0200

    ci: add lint and build baseline

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -1,4 +1,4 @@
-import { useCart } from '../context/CartContext';
+import { useCart } from '../context/useCart';
 
 export default function CartModal() {
   const { cart, cartTotal, isModalOpen, toggleModal, closeModal, changeQty } = useCart();
